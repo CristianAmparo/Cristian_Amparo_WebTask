0c5d14ff02c764243a906b3268f4f00399500498
CristianAmparo <christianamparo132@gmail.com>
2024-03-12 14:00:15 +0800

    slider carousel goods!

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+const swiperEl = document.querySelector('swiper-container');
+
+const params = {
+    centeredSlides: false,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+        enabled: true,
+    },
+    breakpoints: {
+        769: {
+            slidesPerView: 2,
+            slidesPerGroup: 1,
+        },
+        369: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+        },
+    },
+    scrollbar: false,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        clickable: true,
+    },
+};
+
+Object.assign(swiperEl, params)
+
+swiperEl.initialize();
