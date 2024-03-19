ce4b315fc2268a302ebd44e333e8da55c6d48efa
CristianAmparo <christianamparo132@gmail.com>
2024-03-19 11:18:16 +0800

    fixed css issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js,php}"],
   theme: {
     screens: {
       'sm': '640px',
